34ab943805027d2cfd300847fa8c50c9a90c3d5a
John Truong <viajohnt@gmail.com>
2023-03-23 11:30:19 -0600

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    handleToDo(e.target["new-task-description"].value)
+    form.reset()
+  })
 });
+
+function handleToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
